c45436e1c1370ed901159f0896c0dc66b91da01b
puppet <puppet@localhost.local>
2017-10-06 12:11:07 +0000

    Update 'site/profile/manifests/platform/baseline/westrock.pp'

--- site/profile/manifests/platform/baseline/westrock.pp
+++ site/profile/manifests/platform/baseline/westrock.pp
@@ -2,9 +2,12 @@ class profile::platform::baseline::westrock {
 
   # Data Center or Domain specific
   case $::operatingsystemrelease {
-    '2012 R3': {
+    '2012 R2': {
       include ::profile::platform::baseline::windows
       include ::profile::platform::baseline::winmbs
+    }
+    '2016': {
+      include ::profile::platform::baseline::windows
     }
     default: {
       include ::profile::platform::baseline::windows
