8de95905c86ec86c8def0a1856a017ccc263f72c
Greg Loughmiller <gloughmiller@yahoo.com>
2017-10-08 15:39:12 -0400

    Update westrock_windows.pp

--- site/role/manifests/westrock_windows.pp
+++ site/role/manifests/westrock_windows.pp
@@ -1,4 +1,4 @@
-# @summary This role installs IIS and sample content on port 80.
+# Example Role for westrock. Class to be added via node classification
 class role::westrock_windows {
   include ::profile::platform::baseline
 }
